af5e57058832d9853962b11af163a4c8108f2f24
Jordy Shores <jordan.binskin@gmail.com>
2019-11-28 17:14:17 +1100

    set up basic api and routes

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const generic = express.Router();
+const api = express.Router();
+
+let phonebook = [
+  {
+    name: "Gian Gian",
+    number: "0240342",
+    id: 1
+  },
+  {
+    name: "Scott Man",
+    number: "02343123",
+    id: 2
+  },
+  {
+    name: "Garrosh",
+    number: "026452",
+    id: 3
+  },
+  {
+    name: "Pizza man",
+    number: "0243434",
+    id: 4
+  }
+]
+
+app.use(bodyParser.json());
+
+generic.get('/', (req, res) => res.send('<h1>Welcome to the phonebook API</h1>'))
+
+generic.get('/info', (req, res) => res
+  .send(`
+    <p>Phonebook has info for ${phonebook.length} people</p>
+    <p>The time of this request is ${new Date}<p>
+  `)
+)
+
+api.get('/', (req, res) => res.send('Add /phonebook to URL to view the collection'));
+
+api.get('/phonebook', (req, res) => res.json(phonebook));
+
+api.get('/phonebook/:id', (req, res) => {
+  const id = req.params.id;
+  const contact = phonebook.find(contact => contact.id == id);
+  if (contact) {
+    res.json(contact)
+  } else {
+    res.status(400).send('this record does not exist in the collection')
+  }
+})
+
+api.delete('/phonebook/:id', (req, res) => {
+  const id = req.params.id;
+  const person = phonebook.find(contact => contact.id == id)
+  if (person) {
+    phonebook = phonebook.filter(contact => contact.id != id)
+    res.status(200).json(phonebook);
+  }
+  else {
+    res.status(404).json({
+      error: "resource does not exist"
+    })
+  }
+})
+
+api.post('/phonebook', (req, res) => {
+  let content = req.body;
+  if (!content.name || !content.number) {
+    return res.status(400).send('You have provided an incomplete entry, please ensure you provide both name and number in the request');
+  }
+
+  if (phonebook.find(contact => contact.name == content.name)) {
+    return res.status(400).send('This contact already exists')
+  }
+
+  const contact = {
+    name: content.name,
+    number: content.number,
+    date: new Date,
+    id: Math.floor(Math.random() * 1000)
+  }
+
+  phonebook = [...phonebook, contact];
+
+  res.status(200).json(contact)
+})
+
+app.use('/', generic)
+app.use('/api', api)
+const PORT = 3001;
+app.listen(PORT, () => console.log(`listening on port:${PORT}`))
